61c89eacbdc985979cc1bb2d2a905ea9415c8318
aleaneisha <101861826+aleaneisha@users.noreply.github.com>
2022-04-04 13:05:47 +0700

    Create .java

---
+++ .java
@@ -0,0 +1,97 @@
+public class Student {
+   int StudentAge;
+
+   public Student(String name) {
+      // This constructor has one parameter, name.
+      System.out.println("Name of Student is :" + name );
+   }
+
+   public void setAge( int age ) {
+      StudentAge = age;
+   }
+
+   public int getAge( ) {
+      System.out.println("Student's age is :" + StudentAge );
+      return StudentAge;
+   }
+
+   public static void main(String []args) {
+      /* Object creation */
+      Student myStudent = new Student( "Alea Keneisha" );
+
+      /* Call class method to set student's age */
+      myStudent.setAge( 18 );
+
+      /* Call another class method to get student's age */
+      myStudent.getAge( );
+
+      System.out.println("Class : 2SE5" );
+    
+      /* You can access instance variable as follows as well */
+      System.out.println("Variable Value :" + myStudent.StudentAge );
+   }
+}public class Student {
+   int StudentAge;
+
+   public Student(String name) {
+      // This constructor has one parameter, name.
+      System.out.println("Name of Student is :" + name );
+   }
+
+   public void setAge( int age ) {
+      StudentAge = age;
+   }
+
+   public int getAge( ) {
+      System.out.println("Student's age is :" + StudentAge );
+      return StudentAge;
+   }
+
+   public static void main(String []args) {
+      /* Object creation */
+      Student myStudent = new Student( "Alea Keneisha" );
+
+      /* Call class method to set student's age */
+      myStudent.setAge( 18 );
+
+      /* Call another class method to get student's age */
+      myStudent.getAge( );
+
+      System.out.println("Class : 2SE5" );
+    
+      /* You can access instance variable as follows as well */
+      System.out.println("Variable Value :" + myStudent.StudentAge );
+   }
+}public class Student {
+   int StudentAge;
+
+   public Student(String name) {
+      // This constructor has one parameter, name.
+      System.out.println("Name of Student is :" + name );
+   }
+
+   public void setAge( int age ) {
+      StudentAge = age;
+   }
+
+   public int getAge( ) {
+      System.out.println("Student's age is :" + StudentAge );
+      return StudentAge;
+   }
+
+   public static void main(String []args) {
+      /* Object creation */
+      Student myStudent = new Student( "Alea Keneisha" );
+
+      /* Call class method to set student's age */
+      myStudent.setAge( 18 );
+
+      /* Call another class method to get student's age */
+      myStudent.getAge( );
+
+      System.out.println("Class : 2SE5" );
+    
+      /* You can access instance variable as follows as well */
+      System.out.println("Variable Value :" + myStudent.StudentAge );
+   }
+}
